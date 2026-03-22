3c4d8763fe4e6aca5331dd284ca7cf7dc7e196b7
grantpaik100-lgtm <grantpaik100@gmail.com>
2026-03-22 17:14:43 +0900

    page.tsx 업데이트

--- app/pre/page.tsx
+++ app/pre/page.tsx
@@ -2,7 +2,7 @@ import SurveyForm from "../../components/SurveyForm";
 
 export default function PrePage() {
   return (
-    <main className="min-h-screen bg-black text-white">
+    <main className="flex flex-col items-center justify-center h-screen bg-black">
       <SurveyForm />
     </main>
   );
